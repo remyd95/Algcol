7925329d2bf7217b1aadce0e7ad5ddc1b199cc8c
remyd95 <remyduijsens@gmail.com>
2022-06-29 17:07:07 +0200

    testalg

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="algcol",
-    version="0.0.1",
+    version="0.0.2",
     author="Remy Duijsens",
     author_email="remyduijsens@gmail.com",
     description="A library of common algorithms",
